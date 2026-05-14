2efcb0d04c2ff10d4f12b0c856f2bd271714e0df
Ahmad <ahmedmoh4973@gmail.com>
2026-05-14 14:11:24 +0200

    adding WindMeasurement class

---
+++ WindMeasurement.java
@@ -0,0 +1,15 @@
+package algo.winddata;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class WindMeasurement {
+
+        private LocalDate date;
+        private LocalTime time;
+        private double windDirection;
+        private String windDirectionQuality;
+        private double windSpeed;
+        private String windSpeedQuality;
+    }
+

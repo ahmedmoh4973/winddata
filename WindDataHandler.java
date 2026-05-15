4d459fe583593b25f139e3e0a4340d7a6c1fe364
Ahmad <ahmedmoh4973@gmail.com>
2026-05-15 04:19:32 +0200

    adding selected date range including dataFrom, dateTo.

--- WindDataHandler.java
+++ WindDataHandler.java
@@ -127,6 +127,20 @@ public class WindDataHandler {
 	public List<String> highestWindSpeed(LocalDate dateFrom, LocalDate dateTo) {
 
 		//TODO: Implement method
-		return null;  //O(1)
+
+        List<String> result = new ArrayList<>();
+
+        if (dateFrom.isAfter(dateTo)) {
+            return result;
+        }
+
+        Map<LocalDate, List<WindMeasurement>> selectedData =
+                windData.subMap(dateFrom, true, dateTo, true);
+
+        for (LocalDate date : selectedData.keySet()) {
+
+        }
+
+        return result; //O(1)
 	}
 }
